03e0ef4fa6a32098d6726b96396893006c68768d
Yann Mazita <dev.mazita@gmail.com>
2023-03-16 00:24:25 +0100

    Project update

---
+++ src/application/domain.py
@@ -0,0 +1,21 @@
+#from mutagen.easyid3 import EasyID3
+
+class MusicFile:
+    """Music file tags"""
+    def __init__(self) -> None:
+        title: str = ""
+        artist: str = ""
+        album: str = ""
+        albumartist: str = ""
+        date: str = ""
+        description: str = ""
+        discsubtitle: str = ""
+        genre: str = ""
+        labelid: str = ""
+        language: str = ""
+        organization: str = ""
+        performer: str = ""
+        tracknumber: str = ""
+        discnumber: str = ""
+    
+
